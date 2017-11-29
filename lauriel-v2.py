372e58bdd97f9a081fc93a7f07efb32b4e2fc3d9
MrLanwe <m.druzkowski@gmail.com>
2017-11-29 23:38:36 +0100

    trying to figure out the general layout

--- lauriel-v2.py
+++ lauriel-v2.py
@@ -1,19 +1,61 @@
-import bs4
+from bs4 import BeautifulSoup
 import os
+import urllib2
 
 
-class Lauriel():
+class GetUrls():
 
-    def __init__(self, urls, depth, width):
-        self.urls = urls
-        self.depth = depth
-        self.width = width
+    scan_depth = 0
+    scan_width = 0
 
-    def get_addresses(self, urls):
+    def __init__(self, url, depth=2, width=1):
+        self.url = url
+
+    def get_urls(self):
         pass
 
-    def validate_url_syntax(self, urls):
+
+class ValidateUrl(GetUrls):
+
+    """
+    Class for digging out urls from websites
+    Required Inputs: urls
+    Optional Inputs: depth, width
+    """
+
+    correct_syntax_pattern = ".*http.*"
+    ok_status_code = '200'
+
+    def __init__(self):
         pass
 
-    def validate_url_access(self, urls):
+    def validate_url_syntax(self):
         pass
+
+    def validate_url_access(self):
+        pass
+
+
+class Results(GetUrls):
+
+    def __init__(self):
+        pass
+
+    def all_websites(self):
+        pass
+
+
+class Output(Results):
+
+    def __init__(self):
+        pass
+
+    def to_text_file(self):
+        pass
+
+    def to_excel_file(self):
+        pass
+
+    def to_csv_file(self):
+        pass
+
