442e715aeff5be6395508c0009f0d7a0494e8d4e
MrLanwe <m.druzkowski@gmail.com>
2017-11-29 21:39:50 +0100

    initial commit, scheme of lauriel class

---
+++ lauriel-v2.py
@@ -0,0 +1,19 @@
+import bs4
+import os
+
+
+class Lauriel():
+
+    def __init__(self, urls, depth, width):
+        self.urls = urls
+        self.depth = depth
+        self.width = width
+
+    def get_addresses(self, urls):
+        pass
+
+    def validate_url_syntax(self, urls):
+        pass
+
+    def validate_url_access(self, urls):
+        pass
